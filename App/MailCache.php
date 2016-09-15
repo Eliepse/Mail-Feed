f9cbb01b124c8936fc0f6920eebc52551d1fe27f
Elie Meignan <contact@eliepse.fr>
2016-09-15 17:12:02 +0200

    Cache v0.3.0 compatibility

--- App/MailCache.php
+++ App/MailCache.php
@@ -7,12 +7,6 @@ use Eliepse\Cache\Cache;
 class MailCache extends Cache
 {
 
-	public function initConfigs($url = false)
-	{
-		parent::initConfigs();
-
-		if ($url === false && isset($this->cache_config->paths['mail']))
-			$this->cache_path = $this->path_base . $this->cache_config->paths['mail'];
-	}
+	protected $type = 'mail';
 
 }
